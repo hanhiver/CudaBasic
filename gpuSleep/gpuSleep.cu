a86898c7d75a9d6e3ba1aa6b82b2f5a971e12429
hanhiver <13910395157@139.com>
2019-07-09 11:13:34 +0800

    update

---
+++ gpuSleep/gpuSleep.cu
@@ -0,0 +1,64 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+
+#define CHECK(call)                                                            \
+{                                                                              \
+    const cudaError_t error = call;                                            \
+    if (error != cudaSuccess)                                                  \
+    {                                                                          \
+        fprintf(stderr, "Error: %s:%d, ", __FILE__, __LINE__);                 \
+        fprintf(stderr, "code: %d, reason: %s\n", error,                       \
+                cudaGetErrorString(error));                                    \
+        exit(1);                                                               \
+    }                                                                          \
+}
+
+__global__ void gpu_sleep(const int sleep_time)
+{   
+    int tmp = 0;
+    for (int i=sleep_time; i<sleep_time; i++)
+        tmp += i;
+
+    printf("GPU job threadId (%d) done, sleep for %d seconds.\n", threadIdx.x, sleep_time);
+}
+
+
+int main(int argc, char **argv)
+{
+    // set up device. 
+    int dev = 0;
+    cudaDeviceProp dprop;
+    CHECK(cudaGetDeviceProperties(&dprop, dev));
+    printf("%s start at device %d: %s \n", argv[0], dev, dprop.name);
+    CHECK(cudaSetDevice(dev));
+    
+    int sleep_time = 1;
+    if (argc > 1)
+    {
+        sleep_time = atoi(argv[1]);
+    }
+
+    int blocksize = 1;
+    if (argc > 2)
+    {
+        blocksize = atoi(argv[2]);
+    }
+        
+    // execution configuration
+    dim3 block (blocksize);
+    dim3 grid (1);
+
+    // kernel: sleep.
+    gpu_sleep <<<block, grid>>> (sleep_time);
+    
+    sleep(sleep_time);
+
+    // reset device.
+    CHECK(cudaDeviceReset());
+
+    return EXIT_SUCCESS;
+}
+

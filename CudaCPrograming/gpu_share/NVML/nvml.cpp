9b42c79665faed714f4555029b82deca339f4cd4
hanhiver <handongfr@163.com>
2019-11-05 10:47:41 +0800

    update

--- CudaCPrograming/gpu_share/NVML/nvml.cpp
+++ CudaCPrograming/gpu_share/NVML/nvml.cpp
@@ -6,8 +6,8 @@ int main()
     std::cout << "Starting to check the GPU info. " << std::endl; 
 
     int deviceCount = 0; 
-    cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
-
+    //cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
+    auto error_id = cudaGetDeviceCount(&deviceCount);
     if (error_id != cudaSuccess)
     {
         std::cout << "cudaGetDeviceCount returned: " 
@@ -15,8 +15,30 @@ int main()
                   << cudaGetErrorString(error_id) << std::endl; 
         return -1; 
     }
-
     std::cout << "There are " << deviceCount << " CUDA device(s) in the system.\n";
+
+    nvmlDevice_t device; 
+    error_id = nvmlDeviceGetHandleByIndex(0, &device);
+    if (error_id != cudaSuccess)
+    {
+        std::cout << "nvmlDeviceGetHandleByIndex returned: " 
+                  << (int)error_id << " -> " 
+                  << cudaGetErrorString(error_id) << std::endl; 
+        return -1; 
+    }
+    
+    nvmlMemory_t memory; 
+    error_id = nvmlDeviceGetMemoryInfo(device, &memory);
+    if (error_id != cudaSuccess)
+    {
+        std::cout << "nvmlDeviceGetHandleByIndex returned: " 
+                  << (int)error_id << " -> " 
+                  << cudaGetErrorString(error_id) << std::endl; 
+        return -1; 
+    }
+    std::cout << "Total Mem  : " << memory.total << "\n"
+              << "Used Mem   : " << memory.used << "\n"
+              << "Free Mem   : " << memory.free << std::endl; 
 
     return 0; 
 }
